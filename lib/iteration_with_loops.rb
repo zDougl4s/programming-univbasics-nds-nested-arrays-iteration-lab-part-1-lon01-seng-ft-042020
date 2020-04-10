122d4d689459d20c224550df4ad044fd47b5e4bd
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 16:25:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,10 @@ def find_even_values(src)
 row_index = 0
 while count < src.count do
   element_index = 0
-  while element_index <[row_index].count do
+  while element_index <src[row_index].count do
+    if src[row_index][element_index].even?
+      p src[row_index][element_index]
+    end
     element_index += 1
   end
   row_index += 1
